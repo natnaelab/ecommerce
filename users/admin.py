fe00ee34210d760cfc988c7d67f22716ac54c9d5
nat <natnael08@protonmail.com>
2024-05-31 17:34:18 +0300

    Added proxy models

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser as User
+from .models import Customer, Manager, SuperUser
 
-admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Manager)
+admin.site.register(SuperUser)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,49 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-# proxy model
 class CustomUser(AbstractUser):
     class Meta:
         verbose_name = "User"
+        db_table = "user"
+
+
+class ManagerUserManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(groups__name="Manager")
+
+
+class Manager(CustomUser):
+    objects = ManagerUserManager()
+
+    class Meta:
+        proxy = True
+
+
+class CustomerManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .exclude(groups__isnull=False)
+            .exclude(is_superuser=True)
+        )
+
+
+class Customer(CustomUser):
+    objects = CustomerManager()
+
+    class Meta:
+        proxy = True
+
+
+class SuperUserManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_superuser=True)
+
+
+class SuperUser(CustomUser):
+    objects = SuperUserManager()
+
+    class Meta:
+        proxy = True
